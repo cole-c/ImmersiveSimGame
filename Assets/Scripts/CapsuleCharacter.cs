5af4a3301a6d12abce780093cf9b1cf3a15e2a32
cole_c <cole.e.cummings@gmail.com>
2019-07-25 21:16:44 -0500

    Attached camera to character, camera rotates with character

--- Assets/Scripts/CapsuleCharacter.cs
+++ Assets/Scripts/CapsuleCharacter.cs
@@ -5,30 +5,41 @@ using UnityEngine;
 public class CapsuleCharacter : MonoBehaviour
 {
     public float moveSpeed;
-
-    private Vector3 moveInput;
-    private Vector3 moveVelocity;
-    private Rigidbody character;
+    public float rotationSpeed;
+    public Transform cameraTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 5;
+        rotationSpeed = 5000;
 
-        character = GetComponent<Rigidbody>();
+        GameObject cam = GameObject.Find("Main Camera");
+        cameraTransform = cam.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Movement
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3 movementDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) movementDirection += cameraTransform.forward;
+        if (Input.GetKey(KeyCode.S)) movementDirection -= cameraTransform.forward;
+        if (Input.GetKey(KeyCode.A)) movementDirection -= cameraTransform.right;
+        if (Input.GetKey(KeyCode.D)) movementDirection += cameraTransform.right;
 
-        moveVelocity = moveInput * moveSpeed;
+        //TODO change this to jump
+        movementDirection.y = 0f;
+
+        transform.position += movementDirection.normalized * moveSpeed * Time.deltaTime;
+
+        //Keep camera at player's back
+        //if (movementDirection != Vector3.zero)
+        //{
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movementDirection), rotationSpeed * Time.deltaTime);
+        //}
+
+        transform.rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
     }
 
-    private void FixedUpdate()
-    {
-        character.velocity = moveVelocity;
-    }
 }
